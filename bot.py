96fd2cdd00dba517376bdafc186b561702487355
TιJ <78140568+TiJ-code@users.noreply.github.com>
2022-01-24 15:19:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,81 +1,40 @@
-import asyncio
 import discord
-from datetime import datetime
+import asyncio
 
 
-class Bot10A(discord.Client):
+class Edgar(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.loop.create_task(self.statusTask())
+        self.default_channel = client.get_channel(798147945396568098)
 
     async def on_ready(self):
-        print(f'Logging as {self.user}')
+        print(f'Logging in as {self.user}')
 
-    async def statusTask(self):
-        await self.wait_until_ready()
-        while not self.is_closed():
-            await self.change_presence(
-                activity=discord.Activity(name=str(updatecountdown()), type=5),
-                status=discord.Status.do_not_disturb
-            )
-            await asyncio.sleep(15)
+    async def on_message(self, msg):
+        if msg.author.id == client.user.id:
+            return
 
-def updatecountdown():
-    file = open("exams.txt", "r")
-    Lines = file.readlines()
-    title = ""
+        curseWords = ['discode', 'discord.ccom']
 
-    for line in Lines:
-        if line.startswith('!'):
-            pass
-        elif line.startswith('#'):
-            title = line.split(' ')[1]
-        else:
-            delta = line.split('\n')
-            delta = delta[0].split('/')
-            td = int(delta[0])
-            tm = int(delta[1])
-            ty = int(delta[2])
+        if any(word in msg.content for word in curseWords):
+            await msg.delete()
+            await self.default_channel.send(f"LOL! {msg.author.mention} hats erwischt! POGGERS")
+            await send_dm(530758332882092032, "Es hat wieder wen erwischt")
 
-            nowdate = datetime.today().strftime('%d/%m/%y/%H/%M/%S').split('/')
-            nd = int(nowdate[0])
-            nm = int(nowdate[1])
-            ny = int(nowdate[2])
-            nH = int(nowdate[3])
-            nM = int(nowdate[4])
-            nS = int(nowdate[5])
 
-            now = datetime(day=nd, month=nm, year=ny, hour=nH, minute=nM, second=nS)
-            target = datetime(day=td, month=tm, year=ty)
+async def send_dm(member: discord.Member, *, content):
+    channel = await member.create_dm()
+    await channel.send(content)
 
-            over = target - now
-            
-            if int(str(over).split(' ')[0]) < 0:
-                removeExam()
-            if int(str(over).split(' ')[0]) >= 2:
-                rest = "".join(str(over).split(' ')[0] + " Tagen, " + str(over).split(' ')[2]) + " (" + title + ")"
-            else:
-                rest = "".join(str(over).split(' ')[0] + " Tage, " + str(over).split(' ')[2]) + " (" + title + ")"
-            print(rest)
-            return rest
 
 def getRunToken():
-    file = open("exams.txt", "r")
+    file = open('exams.txt', 'r')
     line = file.readline()
+    file.close()
     out = line.replace('!', '')
     out = out.replace(' ', '')
-    file.close()
-    print(out)
     return out
 
-def removeExam():
-    file = open("exams.txt", "r+")
-    lines = file.readlines()
-    lines[1] = ""
-    lines[2] = ""
-    file.write(lines)
-    file.close()
 
-tk = getRunToken()
-client = Bot10A()
-client.run(tk)
+client = Edgar()
+client.run(getRunToken())
